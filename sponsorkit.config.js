025acf54d8357a231c7ceb69a1de559dadcd9b17
GHOSt <ghostdevbusiness@gmail.com>
2022-05-02 12:19:02 +0100

    chore: sponsorkit config

---
+++ sponsorkit.config.js
@@ -0,0 +1,35 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+    patreon: {},
+
+    width: 800,
+    formats: ['png'],
+
+    tiers: [
+        {
+            title: 'Backer',
+            preset: presets.small,
+        },
+        {
+            title: 'Supporter',
+            monthlyDollars: 5,
+            preset: presets.base,
+        },
+        {
+            title: 'Legend',
+            monthlyDollars: 10,
+            preset: presets.medium,
+        },
+        {
+            title: 'Amazing',
+            monthlyDollars: 20,
+            preset: presets.large,
+        },
+        {
+            title: 'God',
+            monthlyDollars: 35,
+            preset: presets.xl,
+        }
+    ]
+})
