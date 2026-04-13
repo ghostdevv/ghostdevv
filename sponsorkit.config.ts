3eb882a5c8d5a6b173c49b6299c080275ddab56a
Willow (GHOST) <git@willow.sh>
2026-04-13 16:08:50 -0400

    deps: update

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presets } from 'sponsorkit';
+import { defineConfig, tierPresets } from 'sponsorkit';
 
 export default defineConfig({
 	providers: ['patreon'],
@@ -10,27 +10,27 @@ export default defineConfig({
 	tiers: [
 		{
 			title: 'Backer',
-			preset: presets.small,
+			preset: tierPresets.small,
 		},
 		{
 			title: 'Supporter',
 			monthlyDollars: 5,
-			preset: presets.base,
+			preset: tierPresets.base,
 		},
 		{
 			title: 'Legend',
 			monthlyDollars: 10,
-			preset: presets.medium,
+			preset: tierPresets.medium,
 		},
 		{
 			title: 'Amazing',
 			monthlyDollars: 20,
-			preset: presets.large,
+			preset: tierPresets.large,
 		},
 		{
 			title: 'God',
 			monthlyDollars: 35,
-			preset: presets.xl,
+			preset: tierPresets.xl,
 		},
 	],
 });
